0b4435cbf2d055995438dbb09b115b407d3bad1a
reweti2020 <59637233+reweti2020@users.noreply.github.com>
2025-04-24 19:19:59 +1000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    // Remove the appDir option completely
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
